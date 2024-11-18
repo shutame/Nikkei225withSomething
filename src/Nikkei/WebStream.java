f2579e6fb50f95ad53165f7cd4ac84edd15f4ad7
TamegaiShuta <shu_tamegai@outlook.jp>
2024-11-19 00:25:15 +0900

    Add WebStream.java

---
+++ src/Nikkei/WebStream.java
@@ -0,0 +1,59 @@
+package Nikkei;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class WebStream {
+    private URL url;
+    private HttpURLConnection connection;
+    private InputStreamReader reader;
+
+    public WebStream(URL url){
+        this.url = url;
+        try{
+            connection = (HttpURLConnection) this.url.openConnection();
+            connection.setRequestProperty(
+                "User-Agent",
+                "Mozilla/5.0 (Windows; U; Windows NT 5.1; ja; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6"
+            );
+            connection.setRequestProperty(
+                "Referer",
+            "http://www.google.com"
+            );
+            connection.setInstanceFollowRedirects(false);
+
+            reader = new InputStreamReader(connection.getInputStream(), "UTF-8");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    // return InputStreamReader object
+    public InputStreamReader reader(){
+        return reader;
+    }
+
+    // return current handling URL
+    public String toString(){
+        return url.toString();
+    }
+    
+    public static void main(String[] args){
+        WebStream parser;
+        try{
+            parser = new WebStream(
+                new URI("http://www.w3.org/").toURL()
+            );
+            System.out.println(parser.toString());
+        }catch(MalformedURLException e){
+            System.err.println(e);
+        }catch(URISyntaxException e){
+            System.err.println(e);
+        }
+    }
+}
